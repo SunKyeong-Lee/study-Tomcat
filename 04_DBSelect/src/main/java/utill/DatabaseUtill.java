d53db6c4c0aa2dd0535a1aec89f502c2519ed9db
sk LEE <znfnfn2140@gmail.com>
2022-11-27 13:01:27 +0900

    1124 DB연결, select

--- 04_DBSelect/src/main/java/utill/DatabaseUtill.java
+++ 04_DBSelect/src/main/java/utill/DatabaseUtill.java
@@ -9,7 +9,7 @@ public class DatabaseUtill {
 			// 데이터베이스와 연결하기 위해서 데이터베이스 주소와 아이디, 비밀번호 연결
 			String dbURL = "jdbc:mysql://localhost:3306/JAVA";
 			String dbID = "root";
-			String dbPassword = "dkffps2140$";
+			String dbPassword = "0000";
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch(Exception e) {
